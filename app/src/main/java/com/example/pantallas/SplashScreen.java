1ece8f6e7a389957199c830861eee2c6f6395dd7
Andreinamilena <andreinamilenasc@gmail.com>
2025-04-11 18:20:22 -0500

    Create SplashScreen.java

---
+++ app/src/main/java/com/example/pantallas/SplashScreen.java
@@ -0,0 +1,42 @@
+package com.example.pantallas;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static final int DURACION_SPLASH = 2000; // 2 segundos
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            SharedPreferences prefs = getSharedPreferences("datos_usuario", MODE_PRIVATE);
+            String categoria = prefs.getString("categoria", "");
+
+            Intent intent;
+            switch (categoria) {
+                case "Deportes":
+                    intent = new Intent(this, DeportesActivity.class);
+                    break;
+                case "Música":
+                    intent = new Intent(this, MusicaActivity.class);
+                    break;
+                case "Cine":
+                    intent = new Intent(this, CineActivity.class);
+                    break;
+                default:
+                    intent = new Intent(this, MainActivity.class);
+                    break;
+            }
+
+            startActivity(intent);
+            finish();
+        }, DURACION_SPLASH);
+    }
+}
