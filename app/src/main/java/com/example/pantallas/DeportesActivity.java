1672ca79d5008e95aa44545a58abfd13923be64c
Andreinamilena <andreinamilenasc@gmail.com>
2025-04-11 18:20:47 -0500

    Create DeportesActivity.java

---
+++ app/src/main/java/com/example/pantallas/DeportesActivity.java
@@ -0,0 +1,24 @@
+package com.example.pantallas;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DeportesActivity extends AppCompatActivity {
+
+    Button btnVolver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deportes);
+
+        btnVolver = findViewById(R.id.btnVolver);
+        btnVolver.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+}
